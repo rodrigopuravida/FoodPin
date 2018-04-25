55a875b972adbf0be86ea42e186c1b7756270039
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2018-04-24 22:32:58 -0700

    Initial Commit

---
+++ FoodPin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by Rodrigo Carballo on 4/24/18.
+//  Copyright © 2018 Rodrigo Carballo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
